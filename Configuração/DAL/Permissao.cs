637d5ed11cc92025bc74a9e0ce82be48931a0311
Luiz005fil <luizfilipirodrigues@hotmail.com>
2023-03-14 16:44:46 -0300

    atualização de dados

---
+++ Configuração/DAL/Permissao.cs
@@ -0,0 +1,37 @@
+﻿using MODELS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Permissao
+    {
+        public void Inserir(Permissao _permissao)
+        {
+
+        }
+        public List<Permissao> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public List<Permissao> BuscarPorNomeUsuaio(string _descricao)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Permissao> BuscarPorId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar(Permissao _permissao)
+        {
+
+        }
+        public void Delete(int _id)
+        {
+
+        }
+    }
+}
---
+++ Configuração/DAL/UsuarioDAL.cs
@@ -0,0 +1,37 @@
+﻿using MODELS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+            
+        }
+        public List<Usuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public List <Usuario> BuscarPorNomeUsuaio(string _nomeUsuario)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Usuario> BuscarPorId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar (Usuario _usuario)
+        {
+
+        }
+        public void Delete (int _id)
+        {
+
+        }
+    }
+}
