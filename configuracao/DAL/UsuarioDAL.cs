f22b343be3b97fc2a651a859c9940f6f7fc0cb8a
Luiz Filipi <luizfilipirodrigues@hotmail.com>
2023-03-15 17:01:28 -0300

    Update UsuarioDAL.cs

--- configuracao/DAL/UsuarioDAL.cs
+++ configuracao/DAL/UsuarioDAL.cs
@@ -36,15 +36,85 @@ namespace DAL
         }
         public List<Usuario> BuscarTodos()
         {
-            throw new NotImplementedException();
+            List<Usuario> usuarios = new List<Usuario>();
+            Usuario usuario;
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Nome, NomeUsuario, Emal, CPF, Ativo, Senha FROM Usuario";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cn.Open();
+
+                using(SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while(rd.Read())
+                    {
+                        usuario = new Usuario();
+                        usuario.Id = Convert.ToInt32(rd["Id"]);
+                        usuario.Name = rd["Nome"].ToString();
+                        usuario.NomeUsuario = rd["NomeUsuario"].ToString();
+                        usuario.Email = rd["Email"].ToString();
+                        usuario.CPF = rd["CPF"].ToString();
+                        usuario.Ativo = Convert.ToBoolean(rd["Ativa"]);
+                        usuario.Senha = rd["Senha"].ToString();
+                        usuarios.Add(usuario);
+                    }
+
+                    return usuarios;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os usuarios");
+            }
         }
         public List <Usuario> BuscarPorNomeUsuaio(string _nomeUsuario)
         {
             throw new NotImplementedException();
         }
-        public List<Usuario> BuscarPorId(int _id)
+        public Usuario BuscarPorId(int _id)
         {
-            throw new NotImplementedException();
+
+            Usuario usuario = new Usuario();
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id,Nome, NomeUsuario, Email, CPF, Ativo, Senha FROM Usuario WHERE Id=@Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Id", _id);
+
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        usuario.Id = Convert.ToInt32(rd["Id"]);
+                        usuario.Name = rd["Nome"].ToString();
+                        usuario.NomeUsuario = rd["NomeUsuario"].ToString();
+                        usuario.Email = rd["Email"].ToString();
+                        usuario.CPF = rd["CPF"].ToString();
+                        usuario.Ativo = Convert.ToBoolean(rd["Ativo"]);
+                        usuario.Senha = rd["Senha"].ToString();
+                    }
+                }
+                return usuario;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar o usuario por id do banco de dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
         public void Alterar (Usuario _usuario)
         {
